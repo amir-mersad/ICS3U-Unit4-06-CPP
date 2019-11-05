c09eb57267bbbf79196b710395fa23d1d46bf806
Ubuntu <ubuntu@ip-172-31-18-12.ec2.internal>
2019-11-05 19:38:22 +0000

    rgb.cpp added

---
+++ rgb.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2019 Amirmahdi Mersad All rights reserved
+//
+// Created by Amir Mersad
+// Created on September 2019
+// This program prints all RGB numbers
+#include <iostream>
+
+int main() {
+    // This function prints all RGB numbers
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+
+    // Process
+    for (red = 0; red < 255; red++) {
+        for (green = 0; green < 255; green++) {
+            for (blue = 0; blue < 255; blue++) {
+                // Output
+                std::cout << "RGB(" << red << "," << green << "," << blue <<
+                          ")" << std::endl;
+            }
+        }
+    }
+}
